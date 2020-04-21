98278f47893a3e1216dad68939d834da606d0a0b
Nina <pinkangelssugar@gmail.com>
2020-04-21 15:29:48 +0300

    My solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('hello mate academy!');
+function sortTable(table) {
+  const thead = table.querySelector('thead');
+  const tbody = table.querySelector('tbody');
+
+  thead.addEventListener('click', (e) => {
+    const th = e.target.closest('th');
+    const column = th.cellIndex;
+    const rows = [...tbody.children];
+
+    rows.sort((rowA, rowB) => {
+      return rowA.cells[column].innerText > rowB.cells[column].innerText
+        ? 1
+        : -1;
+    });
+
+    for (const row of rows) {
+      tbody.append(row);
+    }
+  });
+}
+
+sortTable(document.getElementById('cars'));
